d49eb442a56e01caee921869af91b8e41038d117
MohammedKhaled98 <102185723+MohammedKhaled98@users.noreply.github.com>
2022-07-21 20:41:49 +0300

    Add files via upload

---
+++ signup/cons/insCON.php
@@ -0,0 +1,44 @@
+﻿<?php
+	
+session_start();
+	if(isset($_POST['Name'])){
+	include 'conn.php';
+	$idm=$_SESSION['adare'];
+
+
+
+$Name = $_POST['Name'];
+$major = $_POST['major'];
+$Phone= $_POST['Phone'];
+$city= $_POST['city'];
+$username= $_POST['username'];
+$pass= $_POST['pass'];
+		
+$state = 2 ; 	
+		
+		
+		
+$sql = "INSERT INTO  consultant (Name,major,Phone,city,user,pass,state)
+ VALUES('$Name','$major','$Phone','$city', '$username', '$pass' , '$state' )";
+    		
+    		if( mysqli_query($con,$sql)){
+				 
+
+	$_SESSION['success'] = 'تم اضافة عميل '.$name;
+					
+				}
+				else{
+				
+				echo $Name; 
+					$_SESSION['error'] = $con->error;
+				}
+				
+			
+		/// echo $_SESSION['success'].$_SESSION['error']."hh";	
+		///exit;
+			header('location: thank.html');
+			}
+	
+	
+
+?>
---
+++ signup/insMEM.php
@@ -0,0 +1,63 @@
+﻿<?php
+	
+session_start();
+	if(isset($_POST['Name'])){
+	include 'conn.php';
+	$idm=$_SESSION['adare'];
+
+
+
+$Name = $_POST['Name'];
+$email= $_POST['email'];
+$pass= $_POST['pass'];
+$Phone= $_POST['Phone'];
+$major = $_POST['major'];
+$edulevel= $_POST['edulevel'];
+$city= $_POST['city'];
+$UniName= $_POST['UniName'];
+$gradyear= $_POST['gradyear'];
+$gradst= $_POST['gradst'];
+$gpa= $_POST['gpa'];
+$scale= $_POST['scale'];
+$certificates= $_POST['certificates'];
+$skills= $_POST['skills'];
+$status= $_POST['status'];
+$gpa= $_POST['gpa'];
+
+		
+$state = 4 ; 	
+	//	$sql = "INSERT INTO  members (Name,major,Phone,city,user,pass,state,email,edulevel,UniName,gradyear,scale,certificates,skills,status)
+ //VALUES('$Name','$major','$Phone','$city', '$username', '$pass' , '$state', '$email', '$edulevel', '$UniName', '$gradyear', '$scale', '$certificates', '$skills', '$status' )";
+    		
+		
+		
+$sql = "INSERT INTO  members (Name,major,Phone,city,user,pass,state ,email,edulevel,UniName,gradyear,scale,certificates ,skills,status,gpa,gradst)
+ VALUES('$Name','$major','$Phone','$city', '$username', '$pass' , '$state' , '$email', '$edulevel', '$UniName' , '$gradyear', '$scale', '$certificates' , '$skills', '$status', '$gpa' , '$gradst'  )";
+    		
+    		
+    				
+		
+    				
+		
+
+    		if( mysqli_query($con,$sql)){
+				 
+
+header('location: thanks.html');
+					
+				}
+				else{
+				
+			echo "Error"; 
+		
+				}
+				
+			
+		 echo $_SESSION['success'].$_SESSION['error']."hh";	
+		exit;
+			header('location: thank.html');
+			}
+	
+	
+
+?>
